ad21c9d71d72216ecfceafaa900be2ab4ce74b7f
jprolejko <jprolejko@gmail.com>
2018-11-23 12:15:43 +0100

    First egg_shell

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # VehiclesEvolution
 Implementation of evolutional algorithm for 2D vehicles.
+
+For now, everything should work without installation of anything.
---
+++ cmake-build-debug/CMakeFiles/VehiclesEvolution.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VehiclesEvolution.dir/main.cpp.o"
+  "VehiclesEvolution.pdb"
+  "VehiclesEvolution"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VehiclesEvolution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
