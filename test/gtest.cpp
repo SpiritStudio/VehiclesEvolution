e38964c0da376bf378d671a57e79f1777a5d80c4
--host=jprolejko <--host=jprolejko@gmail.com>
2018-12-04 21:56:11 +0100

    Tests working now

--- test/gtest.cpp
+++ test/gtest.cpp
@@ -4,14 +4,11 @@
 
 #include <gtest/gtest.h>
 #include <math.h>
-#include "include/Physics/Physics.h"
+#include <Physics/Physics.h>
 
-TEST(ExampleMapTest, Map) {
-//    I expected it to work - it didn't
-//    EXPECT_FALSE(Physics::getInstance().allDead());
-    EXPECT_FALSE(sqrt(7.0) == 3);
+TEST(ExamplePhysicsTest, Physics) {
+    EXPECT_FALSE(Physics::getInstance().allDead());
 }
-
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
