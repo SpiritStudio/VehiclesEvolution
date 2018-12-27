03f68c71ec1adec8e6820785fb26aa3d384c9ff2
jprolejko <39591181+jprolejko@users.noreply.github.com>
2018-12-27 23:40:55 +0100

    Feature/egg shell (#17)
    * Hello world
    
    * First egg_shell
    
    * Libs added
    
    * trying to ignore cmake-build*
    
    * trying to ignore cmake-build* once again
    
    * singleton added
    
    * Physics class egg_shell added with Car, CarParameters and Map egg_shells of egg_shells. Example of working temporary added.
    
    * CarParameters added
    
    * Physics classes moved to separated directory
    
    * Few changes to existing classes, Graphics class added as pseudo observer
    
    * minor changes in CarParameters
    
    * EvolutionaryAlgorithm class added
    
    * Physics and graphics synchro, Car as a example box now
    
    * Map as a physical object now
    
    * Minor fix & multiple cars handling added
    
    * Physical wheels with motors added. Car-Car collision turned off
    
    * Wheels are bouncy
    
    * Antialiasing enabled
    
    * Minor fixes with std::vector
    
    * minor fix in CarParameters
    
    * abstract PhysicsObserves deleted
    
    * TGUI added to project
    
    * gtest added to project
    
    * Cars constructor with CarParameters arguments
    
    * Added follow leader implementation
    
    * Added ending vertex to map polyline to enclose it. Density of objects changed
    
    * GUI class added - doesn't display buttons (WHY?)
    
    * Unnecessary GraphicalUserInterface.h executable deleted
    
    * ensureConstantFrameRate method with parameter
    
    * example test added
    
    * Gui now working + minor fixes
    
    * Tests working now
    
    * Tests automatically adding to project
    
    * Needed version of CMake changed
    
    * CMakeLists separated for tests and app. Now everything builds correctly.
    
    * user scripts added
    
    * README.md updated
    
    * Added gui background and fixed gui initialization
    
    * Resizing handling feature added properly
    
    * Added some style
    
    * Added working exit button and moved responsibility of buttons to GUI class
    
    * Added simple generations and car buttons
    
    * Added follow_the_leader checkbox
    
    * Fixed follow the leader when there is no car
    
    * Added concave polygons handling for map
    
    * Added loading map from file
    
    * many changes for pull request
    
    * next wave of pull request changes

---
+++ src/Physics/Map.cpp
@@ -0,0 +1,94 @@
+//
+// Created by SpiritStudio on 28.11.18.
+//
+
+#include <vector>
+#include <fstream>
+
+#include <mapbox/earcut.h>
+
+#include <Physics/Map.h>
+
+Map::Map(b2World &world) {
+    body_def_.position.Set(0.0f, 0.0f);
+
+    body_ = world.CreateBody(&body_def_);
+    // TODO WRAP TRIANGULATION INTO SEPARATE METHOD
+    // MAYBE PARAMETERS - POLYLINE, RETURNS VECTOR OF FIXTURE_DEF?
+    // TODO READ MAP FROM FILE
+    loadFromFile("data/map.txt");
+
+    // The number type to use for tessellation
+    using Coord = double;
+    using N = uint32_t;
+    using Point = std::array<Coord, 2>;
+    std::vector<std::vector<Point>> polygon;
+    std::vector<Point> points;
+
+    for (const auto &point : polyline_)
+    {
+        points.push_back({point.x, point.y});
+    }
+
+
+    polygon.push_back(points);
+
+    std::vector<N> indices = mapbox::earcut<N>(polygon);
+
+    if (indices.size() % 3 != 0)
+    {
+        throw std::runtime_error("Ear cutting algorithm failed!");
+    }
+
+    unsigned long i = 0;
+
+    for (auto it = indices.begin(); it != indices.end(); it += 3)
+    {
+        map_shape_.emplace_back();
+        fixture_def_.emplace_back();
+
+        b2Vec2 triangle[3];
+        Point point = points.at(*it);
+        triangle[0] = b2Vec2(static_cast<float32>(point[0]), static_cast<float32>(point[1]));
+        point = points.at(*(it+1));
+        triangle[1] = b2Vec2(static_cast<float32>(point[0]), static_cast<float32>(point[1]));
+        point = points.at(*(it+2));
+        triangle[2] = b2Vec2(static_cast<float32>(point[0]), static_cast<float32>(point[1]));
+
+        map_shape_.at(i).Set(triangle, 3);
+
+        fixture_def_.at(i).shape = &map_shape_.at(i);
+
+        // Set the box density to be non-zero, so it will be dynamic.
+        fixture_def_.at(i).density = DENSITY_;
+
+        // Override the default friction.
+        fixture_def_.at(i).friction = FRICTION_;
+
+        fixture_def_.at(i).filter.categoryBits = 0x0002;
+        fixture_def_.at(i).filter.maskBits = 0x0001;
+
+        body_->CreateFixture(&fixture_def_.at(i));
+    }
+}
+
+const b2Vec2& Map::getPosition() const {
+    return body_->GetPosition();
+}
+
+const std::vector<b2Vec2>& Map::getPolyline() const {
+    return polyline_;
+}
+
+void Map::loadFromFile(std::string filename) {
+    std::ifstream input(filename);
+
+    polyline_.clear();
+
+    double x, y;
+
+    while (input >> x >> y)
+    {
+        polyline_.emplace_back(x, y);
+    }
+}
